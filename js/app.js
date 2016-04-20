e336941ec2272dd01b8185367507593e700b2a12
Ammon <ammon@tenney.biz>
2016-04-20 06:20:17 -0400

    Add comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 var DEFUALT_MAP_CENTER = { lat: 37.2739675, lng: -104.678212}; // center of the USA
 
+/**
+* @description This is the ViewModel to use with KnockOut.js. This whole app
+* pretty much revolves around this class
+* @constructor
+*/
 function AppViewModel() {
     var t = this;
 
     var map;
 
-    /*
-    @description: Initializes Googles Maps related services for use in this app;
-    this is run when the script is loaded by the browser
+    /**
+    * @description Initializes Googles Maps related services for use in this app;
+    * this is run when the script is loaded by the browser
     */
     function initializeGMaps() {
         map = new google.maps.Map(document.getElementById('map'), {
